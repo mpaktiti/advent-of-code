eed0b595aec72fd6a6088c97f03e15d766b48136
Maria Paktiti <maria.paktiti@gmail.com>
2023-12-02 15:24:04 -0600

    feat: script to prep dir and files for new day

---
+++ prep_day.py
@@ -0,0 +1,35 @@
+import os
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+day = input("Day number to prep for: ")
+
+while not day.isdigit():
+    print("Only digits allowed, try again.")
+    day = input("Day number to prep for: ")
+
+if len(day) == 1:
+    day = "0" + day
+
+# Create directory
+# TODO get puzzle's name from adventofcode.com and use it as part of path name
+path = "./day" + day
+os.mkdir(path) 
+
+# Creates files
+filepath = os.path.join(path, day + '-code.py')
+with open(filepath, 'w') as fp:
+    pass
+filepath = os.path.join(path, day + '-input.txt')
+with open(filepath, 'w') as fp: 
+    pass
+
+print("Directory and files created:")
+list_files(path)
