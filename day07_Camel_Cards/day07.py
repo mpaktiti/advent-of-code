1c11ceff1673f6649f1e458036e1d4ac923c2d49
Maria Paktiti <maria.paktiti@gmail.com>
2023-12-08 16:44:26 -0600

    feat: day 7 - part 2

--- day07_Camel_Cards/day07.py
+++ day07_Camel_Cards/day07.py
@@ -2,7 +2,9 @@ import functools
 
 cards = {}
 types = {'FIVE': 7, 'FOUR': 6, 'FULL_HOUSE': 5, 'THREE': 4, 'TWO': 3, 'ONE': 2, 'HIGH': 1}
-weights = {'A': 5, 'K': 4, 'Q': 3, 'J': 2, 'T': 1}
+weights_no_joker = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1}
+weights_with_joker = {'A': 13, 'K': 12, 'Q': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2, 'J': 1}
+joker_enabled = False
 
 # open file and return contents to caller
 def read_input():
@@ -14,7 +16,6 @@ def read_cards():
     for line in read_input():
         temp = line.strip().split()
         cards[temp[0]] = {'bid': int(temp[1])}
-    print('Cards read from file: ', cards)
 
 # helper function that checks if the characters of card, excluding same_char, make a pair or are different
 def remaining_chars_make_pair(card, same_char):
@@ -73,7 +74,6 @@ def is_one(card):
 # for each card find its type and set it as part of the dictionary
 def calculate_type():
     for card in cards:
-        print(card)
         if is_five(card):
             cards[card]['type'] = 'FIVE'
         elif is_four(card):
@@ -88,65 +88,78 @@ def calculate_type():
             cards[card]['type'] = 'ONE'
         else:
             cards[card]['type'] = 'HIGH'
-    print('Cards with type: ', cards)
+
+def update_type_for_joker():
+    for card in cards:
+        if 'J' in card and cards[card]['type'] != 'FIVE':
+            num_of_jokers = card.count('J')
+            if is_four(card) or is_full_house(card):
+                 # 3 options here: it's either J????, ?JJJJ, J???J or ?JJJ? (2JJJ2) -> all turn into 5
+                cards[card]['type'] = 'FIVE'
+            elif is_three(card):
+                # it can turn into a full house, or a four (but four is better so it will never turn into full house)
+                # ???Jx (->4), JJJxy (->4)
+                cards[card]['type'] = 'FOUR'
+            elif is_two(card):
+                # xyJyx(->foul), yJxJy(->4)
+                if num_of_jokers == 1:
+                    cards[card]['type'] = 'FULL_HOUSE'
+                elif num_of_jokers == 2:
+                    cards[card]['type'] = 'FOUR'
+            elif is_one(card):
+                cards[card]['type'] = 'THREE'
+            else: # if it's high then plus the joker makes it a one pair
+                cards[card]['type'] = 'ONE'
 
 def weight_chars(x, y):
+    weights = weights_with_joker if joker_enabled else weights_no_joker
     if weights[x] < weights[y]:
         return -1
     elif weights[x] > weights[y]:
+        return 1
+    return 0
+
+def compare_ranks(x_rank,y_rank):
+    if x_rank < y_rank:
+        return -1
+    elif x_rank > y_rank:
         return 1
     else:
         return 0
 
 def compare(x, y):
-    # 1st level ranking
-    x_type_rank = types[cards[x]['type']]
+    # 1st level ranking: compare the strength of the cards types
+    x_type_rank = types[cards[x]['type']] 
     y_type_rank = types[cards[y]['type']]
-    if x_type_rank < y_type_rank:
-        return -1
-    elif x_type_rank > y_type_rank:
-        return 1
+    rank_res = compare_ranks(x_type_rank, y_type_rank)
+    if rank_res != 0:
+        return rank_res
     
-    # if still here than we proceed to 2nd level ranking
+    # if still here than we proceed to 2nd level ranking (ignore jokers even if the flag is enabled)
     i = -1
     for char in x:
         i += 1
         if char == y[i]:
             continue
-        elif char.isalpha(): 
-            if y[i].isdigit():
-                return 1
-            return weight_chars(char, y[i])
-        elif char.isdigit():
-            if y[i].isalpha():
-                return -1
-            # both chars are digits
-            if int(char) < int(y[i]):
-                return -1
-            elif int(char) > int(y[i]):
-                return 1
-            else:
-                return 0
+        chars_weight = weight_chars(char, y[i])
+        if chars_weight == 0:
+            continue
+        return chars_weight
+    return 0
 
 def calculate_strength():
     # first ranking: type
     # if the type is the same then we do the 2nd level ranking
     sorted_cards = sorted(cards, key=functools.cmp_to_key(compare), reverse=True)
-    print(sorted_cards)
     current_rank = len(sorted_cards)
     for item in sorted_cards:
         cards[item]['rank'] = current_rank
         current_rank -= 1
-    print(cards)
 
 def total_winnings():
     total = 0
     for card in cards:
-        print('card: ', cards[card])
-        print('bid: ', cards[card]['bid'])
-        print('rank: ', cards[card]['rank'])
         total += cards[card]['bid'] * cards[card]['rank']
-        print('total = ', total)
     return total
 
 read_cards()
@@ -154,3 +167,8 @@ calculate_type()
 calculate_strength()
 print('Total winnings: ', total_winnings())
 
+# enabled joker flag, re-calculate types accounting for joker, and find the new strengths and sum of winnings
+joker_enabled = True
+update_type_for_joker()
+calculate_strength()
+print('Total winnings with joker: ', total_winnings())
