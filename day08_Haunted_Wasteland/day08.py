ff197ed09565de94905aaff0614d30c9bac17100
Maria Paktiti <maria.paktiti@gmail.com>
2023-12-09 11:42:15 -0600

    feat: day 8 - part 2

--- day08_Haunted_Wasteland/day08.py
+++ day08_Haunted_Wasteland/day08.py
@@ -1,42 +1,39 @@
-map = {}
-
-# open file and return contents to caller
-def read_input():
-    with open("input.txt") as input_file:
-        return input_file.readlines()
+import re
+import math
 
 # read cards from file and copy them to memory
 def read_cards():
-    is_first_line = True
-    for line in read_input():
-        if is_first_line:
-            instructions = line.strip()
-            is_first_line = False
-            continue
-        elif line.isspace():
-            continue
-        temp = line.strip().split('=')
-        remove_parenthesis = temp[1].strip()[1:-1]
-        temp2 = remove_parenthesis.strip().split(',')
-        map[temp[0].strip()] = [temp2[0].strip(), temp2[1].strip()]
-    return instructions
+    desert_map = {}
+    file = open("input.txt").readlines() # TODO replace read_input() with this
+    instructions = list(file[0].strip()) # first line is instructions
+    # 3+ lines are the desert map nodes
+    desert_map = {
+        x[1]: (x[2], x[3])
+        for x in (re.match(r"(\w+)\s*=\s*\((\w+), (\w+)\)", line) for line in file[2:])
+    }
+    return instructions, desert_map
 
-def read_map(instructions):
-    current_pos = 'AAA'
-    target_pos = 'ZZZ'
+def read_map_as_human(instructions, desert_map, current_pos):
     steps_taken = 0
     i = 0
-    while current_pos != target_pos:
+    condition = True
+    while condition:
         direction = instructions[i]
         i += 1
         if i == len(instructions): # if you ran out of instructions repeat the same
             i = 0
-        if direction == 'L':
-            current_pos = map[current_pos][0]
-        else:
-            current_pos = map[current_pos][1]
+        current_pos = desert_map[current_pos][direction == 'R']
         steps_taken += 1
+        condition = current_pos != 'ZZZ' if current_pos == 'AAA' else not current_pos.endswith("Z")
     return steps_taken
 
-instructions = read_cards()
-print('number of steps to reach ZZZ: ', read_map(instructions))
+def read_map_as_ghost(instructions, desert_map):
+    count = []
+    for next_node in [n for n in desert_map if n.endswith("A")]:
+        steps_for_next_node = read_map_as_human(instructions, desert_map, next_node)
+        count.append(steps_for_next_node)
+    return math.lcm(*count)
+
+instructions, desert_map = read_cards()
+print('number of human steps to reach ZZZ: ', read_map_as_human(instructions, desert_map, 'AAA'))
+print('number of ghost steps to reach ZZZ: ', read_map_as_ghost(instructions, desert_map))
